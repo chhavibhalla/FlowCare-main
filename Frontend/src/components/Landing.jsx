d5ec1f89696849f2db87380c3de395b8f418d72b
Parwathi-Jayaram <parwathijayaram1@gmail.com>
2025-01-22 19:11:43 +0530

    nutriguide

--- Frontend/src/components/Landing.jsx
+++ Frontend/src/components/Landing.jsx
@@ -26,6 +26,9 @@ import {
   ArrowRight,
   UserCircle,
   UsersRound,
+  CalendarHeart,
+  Church,
+  Package,
 } from "lucide-react";
 
 export function Landing() {
@@ -340,6 +343,45 @@ export function Landing() {
       <p className="text-sm text-gray-700 dark:text-gray-300 mt-2">
         A safe, anonymous space for peer support, discussions, and expert Q&A on
         menstrual health and related topics.
+      </p>
+    </div>
+    <div className="flex flex-col items-start p-6 bg-white dark:bg-gray-800 rounded-lg shadow-md transform transition-all duration-300 hover:scale-105 hover:shadow-lg"
+    >
+      <CalendarHeart className="h-8 w-8 text-pink-600 dark:text-pink-400 mb-4" />
+      <h4
+        className="text-lg font-semibold text-pink-600 cursor-pointer hover:underline"
+        onClick={() => (window.location.href = "/forums")}
+      >
+        Nutrition and Wellness
+      </h4>
+      <p className="text-sm text-gray-700 dark:text-gray-300 mt-2">
+      Explore essential tips and guidance on maintaining nutrition and wellness during your menstrual cycle, including insights into managing PCOS and PCOD effectively. Stay healthy and balanced throughout every phase.
+      </p>
+    </div>
+    <div className="flex flex-col items-start p-6 bg-white dark:bg-gray-800 rounded-lg shadow-md transform transition-all duration-300 hover:scale-105 hover:shadow-lg"
+    >
+      <Church className="h-8 w-8 text-pink-600 dark:text-pink-400 mb-4" />
+      <h4
+        className="text-lg font-semibold text-pink-600 cursor-pointer hover:underline"
+        onClick={() => (window.location.href = "/forums")}
+      >
+        NGOs
+      </h4>
+      <p className="text-sm text-gray-700 dark:text-gray-300 mt-2">
+      Discover and connect with NGOs dedicated to women’s health, empowerment, and well-being. Together, we can build a supportive community for women everywhere.
+      </p>
+    </div>
+    <div className="flex flex-col items-start p-6 bg-white dark:bg-gray-800 rounded-lg shadow-md transform transition-all duration-300 hover:scale-105 hover:shadow-lg"
+    >
+      <Package className="h-8 w-8 text-pink-600 dark:text-pink-400 mb-4" />
+      <h4
+        className="text-lg font-semibold text-pink-600 cursor-pointer hover:underline"
+        onClick={() => (window.location.href = "/forums")}
+      >
+      ShareJoy
+      </h4>
+      <p className="text-sm text-gray-700 dark:text-gray-300 mt-2">
+      Make a difference by donating sanitary pads through our curated platform, ensuring access to menstrual hygiene for those in need. Sharing joy starts here!
       </p>
     </div>
   </div>
